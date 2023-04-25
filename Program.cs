938df79befd2862c3de13e55b72eed12e41cb0b0
jerometrancart <jerome.trancart@gmail.com>
2023-04-25 13:38:19 +0200

    writer done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using ExerciceXmlWriter;
+
+var parametres = new XmlWriterSettings
+{
+    Indent = true
+};
+var writer = XmlWriter.Create("fichier.xml", parametres);
+
+writer.WriteStartElement("Personne");
+
+    writer.WriteElementString("Nom", "MOMMER");
+    writer.WriteElementString("Nom", "Christophe");
+
+    writer.WriteStartElement("DateDeNaissance");
+        writer.WriteValue("1988-12-18T00:00:00");
+    writer.WriteEndElement();
+
+    writer.WriteStartElement("Taille");
+        writer.WriteValue("181");
+    writer.WriteEndElement();
+
+writer.WriteEndElement();
+
+writer.Flush();
